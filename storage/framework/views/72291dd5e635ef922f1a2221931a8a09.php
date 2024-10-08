9f6fd9d5397085293a46de60bb0f58e7e68464ac
Guilherme <goliva12@hotmail.com>
2024-10-08 13:27:44 -0300

    Inicio do projeto utilizando laravel

---
+++ storage/framework/views/72291dd5e635ef922f1a2221931a8a09.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeremias</title>
+</head>
+
+<body>
+    oi jeremias
+
+    <?php if(auth()->guard()->check()): ?>
+        <span>Voce esta autenticado</span>
+    <?php endif; ?>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\plataforma_freelancer\resources\views/teste/jeremias.blade.php ENDPATH**/ ?>
